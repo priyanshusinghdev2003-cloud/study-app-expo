370dd7c1364931f3d69991b0c20be0838a1d8ef9
Priyanshu Singh <priyanshusinghdev2003@gmail.com>
2026-03-23 10:46:03 +0530

    feat: Set up Sentry error monitoring, Clerk authentication, root navigation, and add reusable empty state components.

--- src/app/(tabs)/explore.tsx
+++ src/app/(tabs)/explore.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const ExploreScreen = () => {
   return (
-    <View>
+    <SafeAreaView className="flex-1 bg-background">
       <Text>ExploreScreen </Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,28 @@
 import { ClerkProvider } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import * as Sentry from "@sentry/react-native";
 import { Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import "../../global.css";
+
+Sentry.init({
+  dsn: "https://2bd054fd1d4cc3542ebab08e7671675f@o4511012985700352.ingest.de.sentry.io/4511080621801552",
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 
